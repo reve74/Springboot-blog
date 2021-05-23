0a0168a2e08aae16d9a24de8f7a14203853619a5
reve74 <reve99@naver.com>
2021-05-23 19:35:21 +0900

    15

--- src/main/java/com/cos/blog/test/httpControllerTest.java
+++ src/main/java/com/cos/blog/test/httpControllerTest.java
@@ -16,6 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class httpControllerTest {
 	
+	private static final String  TAG = "HttpControllerTest: ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+  "getter : " +m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+  "setter : " +m.getUsername());
+		return "lombok test 완료";
+	}
+	
 	// 인터넷브라우저 요청은 무조건 get 요청밖에 할 수 없음
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
