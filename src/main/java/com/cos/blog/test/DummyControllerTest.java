2e2cead1a4816f63fa00d088ed57892cf1da20cf
reve74 <reve99@naver.com>
2021-05-29 08:05:57 +0900

    28

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,11 +1,18 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -18,6 +25,37 @@ public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requesUser) { // json 데이터를 요청 => Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줌. 
+		System.out.println("id : " +id);
+		System.out.println("password : " +requesUser.getPassword()); 
+		System.out.println("email : " +requesUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()-> {
+			return new IllegalArgumentException("수정에 실패하였습니다");
+		});
+		user.setPassword(requesUser.getPassword());
+		user.setEmail(requesUser.getEmail());
+
+		userRepository.save(user);
+		return null;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	//한페이지당 2건에 데이터를 리턴
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUsers =  userRepository.findAll(pageable);
+		
+		List<User> users = pagingUsers.getContent();
+		return users;
+	}
 	
 	// {id} 주로솔 파라미터를 전달받을 수 있음
 	// http://localhost:8000/blog/dummy/user/5
