f4175b94b9638b64150423d37cd40a30ed20a698
reve74 <reve99@naver.com>
2021-05-29 03:13:05 +0900

    26

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +25,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder // 빌더 패턴 !
 @Entity //User 클래스가 MySQL에 테이블이 생성이 된다.
+// @DynamicInsert // insert 시 null인 필드를 제외
 public class User {
 	
 	@Id // Primary key
@@ -37,8 +41,10 @@ public class User {
 	@Column(nullable =  false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")
-	private String role; //Enum을 쓰는게 좋다. // Admin, user, manager
+	// @ColumnDefault("user")
+	// DB는 RoleType이 없음
+	@Enumerated(EnumType.STRING)
+	private RoleType role; //Enum을 쓰는게 좋다. // Admin, user, manager
 	
 	@CreationTimestamp // 시간이 자동 입력
 	private Timestamp createDate;
