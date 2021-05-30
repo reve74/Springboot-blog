e97ac07ae31abe3e413113763614761761558108
reve74 <reve99@naver.com>
2021-05-30 23:17:26 +0900

    28

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,11 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	// save 함수는 id를 전달하지 않으면 insert를 해주고
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	// save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다
+	
+	@Transactional
 	@PutMapping("/dummy/user/{id}")
 	public User updateUser(@PathVariable int id, @RequestBody User requesUser) { // json 데이터를 요청 => Java Object(MessageConverter의 Jackson라이브러리가 변환해서 받아줌. 
 		System.out.println("id : " +id);
@@ -37,8 +43,11 @@ public class DummyControllerTest {
 		});
 		user.setPassword(requesUser.getPassword());
 		user.setEmail(requesUser.getEmail());
-
-		userRepository.save(user);
+		
+		
+		//userRepository.save(user);
+		
+		//더티 체킹
 		return null;
 	}
 	
