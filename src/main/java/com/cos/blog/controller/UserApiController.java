879bffcf9e8a6df7382ffa6f010152c45a8749db
reve74 <reve99@naver.com>
2021-06-11 23:54:43 +0900

    46

--- src/main/java/com/cos/blog/controller/UserApiController.java
+++ src/main/java/com/cos/blog/controller/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,4 +27,18 @@ public class UserApiController {
 		userService.회원가입(user);
 		return new  ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
+	
+	@PostMapping("//blog/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+		System.out.println("UserApiController : login호출됨");
+		User principal = userService.로그인(user); // principal 접근주체
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+	}
 }
+
+
+
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,6 +1,5 @@
 package com.cos.blog.service;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -8,6 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
@@ -23,4 +23,10 @@ public class UserService {
 	public void 회원가입(User user) {
 			userRepository.save(user);
 	}
+	
+	@Transactional(readOnly = true) // Select 할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+	public User 로그인(User user) {
+		 return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+	}
 }
+
