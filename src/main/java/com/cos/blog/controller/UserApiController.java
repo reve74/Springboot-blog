ea5fd2ef09ee9dd7abe7f31b508c440517abb8ad
reve74 <reve99@naver.com>
2021-06-13 05:07:21 +0900

    스프링 시큐리티설정

--- src/main/java/com/cos/blog/controller/UserApiController.java
+++ src/main/java/com/cos/blog/controller/UserApiController.java
@@ -19,7 +19,7 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("/api/user")
+	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save 호출 됨");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
@@ -27,6 +27,8 @@ public class UserApiController {
 		userService.회원가입(user);
 		return new  ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
+	
+	
 	
 //	@PostMapping("/api/user/login")
 //	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -10,10 +10,12 @@ import com.cos.blog.model.User;
 // 자동으로 bean등록이 된다.
 // @Repository // 생략가능
 public interface UserRepository extends JpaRepository<User, Integer> {
-	// JPA Naming 쿼리
-	//SELECT * FROM user WHRERE username = ? AND password = ?;
-	User findByUsernameAndPassword(String username, String password);
-	
-//	@Query(value = "SELECT * FROM user WHRERE username = ?1 AND password = ?2", nativeQuery = true)
-//	User login(String username, String password);
+
 }
+
+// JPA Naming 쿼리
+//SELECT * FROM user WHRERE username = ? AND password = ?;
+//User findByUsernameAndPassword(String username, String password);
+
+//@Query(value = "SELECT * FROM user WHRERE username = ?1 AND password = ?2", nativeQuery = true)
+//User login(String username, String password);
