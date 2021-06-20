f1172c447354f4820b7e8a1c97ae59d4185fb160
reve74 <reve99@naver.com>
2021-06-20 23:46:15 +0900

    50

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,16 +15,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한  및 인증을 미리 체크하겠다는 뜻.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean // IoC가 되요!
 	public BCryptPasswordEncoder encoderPWD() {
-		String encPassword = new BCryptPasswordEncoder().encode("1234");
 		return new BCryptPasswordEncoder();
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() // csrf 토큰 비활성화(테스트 시 걸어두는게 좋음)
 			.authorizeRequests()
-				.antMatchers("/auth/**")
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -6,11 +6,15 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
+
+import ch.qos.logback.core.encoder.Encoder;
 
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IOC 해준다
 @Service
@@ -19,8 +23,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional
 	public void 회원가입(User user) {
+			String rawPassword = user.getPassword(); // 1234 원문 
+			String encPassword = encoder.encode(rawPassword); // 해쉬
+			user.setPassword(encPassword);
+			user.setRole(RoleType.USER);
 			userRepository.save(user);
 	}
 	
