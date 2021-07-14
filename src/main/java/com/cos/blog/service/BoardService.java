6bbc87d1a0f1d429e3b9ae5ae81cb91b2ae09979
reve74 <reve99@naver.com>
2021-07-15 04:10:35 +0900

    글목록보기

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.cos.blog.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -30,6 +32,10 @@ public class BoardService {
 		board.setCount(0);	
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+
+	public List<Board> 글목록() {
+		return boardRepository.findAll();
 	}
 	
 
