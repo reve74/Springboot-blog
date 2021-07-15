819b1709fd1a4fa036ed3b782f4bb7a6a59efd95
reve74 <reve99@naver.com>
2021-07-16 02:46:59 +0900

    수정

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,14 @@ public class UserApiController {
 		return new  ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
 	
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user) {
+		userService.회원수정(user);
+		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
+		// 하지만 세션값은 변경되지 않은 상태기 때문에 직접 세션값을 변경해줘야함.
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
 	
 	
 //	@PostMapping("/api/user/login")
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,9 +3,12 @@ let index = {
 		$("#btn-save").on("click", () => {	// function(){} ,()=> {}this를 바인딩 하기 위해서
 			this.save();
 		});
+		$("#btn-update").on("click", () => {	// function(){} ,()=> {}this를 바인딩 하기 위해서
+			this.update();
+		});		
 
-	}
-	, save: function() {
+	},
+	save: function() {
 		//alert('user의 save함수 호출됨');
 		let data = {
 			username: $("#username").val(),
@@ -27,6 +30,28 @@ let index = {
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
 			//console.log(resp);
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+		
+	},
+
+	update: function() {
+		let data = {
+			id: $("#id").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		};
+	
+		$.ajax({
+			type: "PUT",
+			url: "/user",
+			data: JSON.stringify(data), 
+			contentType: "application/json; charset=utf-8", 
+			dataType: "json" 
+		}).done(function(resp){
+			alert("회원수정이 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
