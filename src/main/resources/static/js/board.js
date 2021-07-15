8429caf3c612df941676944114439784d71e9a7f
reve74 <reve99@naver.com>
2021-07-15 23:55:14 +0900

    수정

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,9 +3,14 @@ let index = {
 		$("#btn-save").on("click", () => {	// function(){} ,()=> {}this를 바인딩 하기 위해서
 			this.save();
 		});
-
-	}
-	, save: function() {
+		$("#btn-delete").on("click", () => {	// function(){} ,()=> {}this를 바인딩 하기 위해서
+			this.deleteById();	
+		});
+		$("#btn-update").on("click", () => {	// function(){} ,()=> {}this를 바인딩 하기 위해서
+			this.update();	
+		});
+	},
+	 save: function() {
 		//alert('user의 save함수 호출됨');
 		let data = {
 			title: $("#title").val(),
@@ -26,7 +31,45 @@ let index = {
 			alert(JSON.stringify(error));
 		});
 		
+	},
+	deleteById: function() {	
+		let id = $("#id").text();
+		
+		$.ajax({
+			// 회원가입 수행 요청
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json" 
+		}).done(function(resp){
+			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		});
+	},
+	update: function() {	
+		let id = $("#id").val();
+		
+		let data = {
+				title: $("#title").val(),
+				content: $("#content").val(),
+			};
+			
+			$.ajax({
+				// 회원가입 수행 요청
+				type: "PUT",
+				url: "/api/board/"+id,
+				data: JSON.stringify(data), 
+				contentType: "application/json; charset=utf-8", 
+				dataType: "json" 
+			}).done(function(resp){
+				alert("글 수정이 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(error){
+				alert(JSON.stringify(error));
+			});
 	}
+	
 	
 }
 
